21ae45d0f301faaabc1ceaf6caa1e79621415571
Gautam Saini <73359023+Gautam1834@users.noreply.github.com>
2021-08-05 23:58:07 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -41,13 +41,12 @@ def create_feed_checker(feed_url):
         entry = FEED.entries[0]
         if entry.id != db.get_link(feed_url).link:
                        # ↓ Edit this message as your needs.
-            message = f"**{entry.title}**\n"
-            message1 = f"/mir1 {entry.link}"
-            message2 = f"/leech {entry.link}"
+            message = f"/mir1 {entry.link}\n\n**{entry.title}**\n"
+        
             try:
                 app.send_message(log_channel, message)
-                app.send_message(log_channel, message1)
-                app.send_message(log_channel, message2)
+                #app.send_message(log_channel, message1)
+                #app.send_message(log_channel, message2)
                 db.update_link(feed_url, entry.id)
             except FloodWait as e:
                 print(f"FloodWait: {e.x} seconds")
